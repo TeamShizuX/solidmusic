517331660124efe5497eaf47ad30fc396e7bfeaa
TeamShizu <88240053+TeamShizuX@users.noreply.github.com>
2021-11-17 14:48:11 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -12,11 +12,11 @@ class Config:
     API_ID = int(getenv("API_ID", "0"))
     API_HASH = getenv("API_HASH", "abc123")
     BOT_TOKEN = getenv("BOT_TOKEN", "1234:abcd")
-    SESSION = getenv("SESSION", "session")
+    SESSION = getenv("SESSION_NAME", "session")
     OWNER_ID = int(getenv("OWNER_ID", "1952053555"))
-    SUPPORT = getenv("SUPPORT", "https://t.me/solidprojects_chat")
-    CHANNEL = getenv("CHANNEL", "https://t.me/solidprojects")
-    UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/DoellBarr/solidmusic")
+    SUPPORT = getenv("GROUP_SUPPORT", "https://t.me/solidprojects_chat")
+    CHANNEL = getenv("UPDATES_CHANNEL", "https://t.me/solidprojects")
+    UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/TeamShizuX/solidmusic")
 
 
 config = Config()
